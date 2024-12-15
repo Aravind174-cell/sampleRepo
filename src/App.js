91048b547894887cbcd8b5dbc731c1567ba21f72
Aravind175-cell <avulaaravind00789@gmail.com>
2024-12-15 15:31:46 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-
-
-import React,(useState) from 'react'
+import React from 'react'
 
 const App = () => {
-  const [task,setTask] = useState("");
   return (
     <div>
-      <center>
-        <div className="card">
-          <div className="card-body">
-          <h5 className="card-title">Todo Management Application</h5>
-           <form>
-            <input type="text" /> &nbsp; &nbsp;
-            <input type="submit" value="Add" name="Add" />
-           </form>
-          </div>
-
-        </div>
-      </center>
       
     </div>
   )
